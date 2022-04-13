7f03b95e9bc6c1a16cfc71c3ebb71a33a2429f95
zuenamash <mnachariazuenah@gmail.com>
2022-04-13 21:30:43 +0300

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,50 @@
+fun main() {
+    var name="Zuenah  Macharia"
+    var age=20
+
+
+    println("=======================")
+    var she = "`-._,-'"
+    var timesToRepeat = 4
+    println ("sing,sing,sing")
+    println("Happy birthday ${name}")
+    println("x4")
+
+    println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+
+    println("   ,,,,,   ")
+    println("   |||||   ")
+    println(" =========")
+    println("@@@@@@@@@@@")
+    println("{~@~@~@~@~}")
+    println("@@@@@@@@@@@")
+    println("")
+    println(":::::::::::::::::::::::")
+    println("=======================")
+    println("You are already ${age}")
+    println("${age} is the very best years to celebrate")
+
+    println("age")
+
+
+
+    println("Blow as many years and add more  ${age} as posible")
+
+}
+
+fun printBorder(border: String, timesToRepeat: Int) {
+    repeat(timesToRepeat) {
+        println()
+    }
+    println()
+}
+fun printCake(age: Int) :Int {
+    var reapply = { age }
+    return(age)
+}
+
+
+
+
+
+
